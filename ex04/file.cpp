ee8eb8e5f3b73646c646847829a87e1e177d1fbb
aroualid <aroualid@student.42.fr>
2025-03-03 14:27:29 +0100

    ex04 finish

--- ex04/file.cpp
+++ ex04/file.cpp
@@ -2,42 +2,18 @@
 #include <fstream>
 #include <iostream>
 
-int findStringPos (std::string line, std::string s1, int start)
-{
-	int first = line.find(s1, start);
-	return (first);
-}
-
 void fillSecondFile (std::string line, std::string s1, std::string s2, std::string secondfile)
 {
 	std::ofstream ofs(secondfile.c_str());
 	int len = s1.length();
-	int lineLen = line.length();
-	int	start = 0;
-	int	pos = findStringPos(line, s1, start);	
-	std::cout << " pos = " << pos << "\n";
-	std::cout << " start = " << start << "\n";	
-	for (int i = start; i < pos; i++)
+	int start = 0;
+	unsigned long pos = line.find(s1, start);
+	while (pos != std::string::npos)
 	{
-		ofs << line[i];
-		std::cout << " i = " << i << "\n";
-		if (i + 1 == pos )
-		{
-			
-			ofs << s2;
-			start = pos + len;
-			pos = findStringPos(line, s1, start);
-			std::cout << " pos = " << pos << "\n";
-			std::cout << " start = " << start << line[start] << "\n";
-		}
-		if (start < lineLen && i < lineLen && pos == -1)
-		{
-			while (start < lineLen)
-			{
-				ofs << line[start];
-				start++;
-			}
-		}
+		line.erase (pos, len);
+		line.insert (pos, s2);
+		start = pos + s2.length();
+		pos = line.find(s1, start);
 	}
+	ofs << line;
 }
-
--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,12 +6,16 @@ int main(int argc, char **av)
 	{
 		std::ifstream ifs(av[1]);
 		if (!ifs)
-			std::cout << "error \n";
+		{
+			std::cout << "error wrong file \n";
+			return -1 ;
+		}
 		std::string line;
 		std::getline(ifs, line, '\0');
-		std::cout << line << "\n";
 		std::string secondfile(av[1]);
 		secondfile  += ".replace";
 		fillSecondFile(line, av[2], av[3], secondfile);
 	}
+	else
+		std::cout << "Wrong Number of arg \n";
 }
